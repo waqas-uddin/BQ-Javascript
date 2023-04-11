49b95f8de77fa39804c94b559808b20a48290653
Waqas Uddin <wa17555@gmail.com>
2023-04-12 00:37:20 +0500

    FEAT: JS Files

---
+++ scripts/app.js
@@ -0,0 +1,5 @@
+var Z_Percentage = 0.025
+var User_Input = +prompt("Enter your net worth on which Zakat is applicable: ")
+var Result = Z_Percentage*User_Input
+
+alert("Your Zakat Value Is : " + Result)
